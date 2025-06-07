fc01172263b422c29db0095da79cf5d107128ff7
“Srividyagit config user.email “kasu.srividya@gmail.com <kasu.srividya@gmail.com>
2025-06-07 14:24:07 +0200

    updated menu header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,14 +34,16 @@ const Header = () => {
       <nav className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
         <Link to="/" className="nav-link" onClick={closeMenu}>Home</Link>
         <Link to="/about" className="nav-link" onClick={closeMenu}>About</Link>
-        <Link to="/locations" className="nav-link" onClick={closeMenu}>Locations</Link>
         <Link to="/gallery" className="nav-link" onClick={closeMenu}>Gallery</Link>
-        <Link to="/events" className="nav-link" onClick={closeMenu}>Events</Link>
         <Link to="/registration" className="nav-link" onClick={closeMenu}>Registration</Link>
-        <Link to="/learn" className="nav-link" onClick={closeMenu}>Learn</Link>
-        <Link to="/volunteer" className="nav-link" onClick={closeMenu}>Volunteer</Link>
         <Link to="/donate" className="nav-link" onClick={closeMenu}>Donate</Link>
       </nav>
+
+      <div className="logo-container logo-container-right">
+        <Link to="/" onClick={closeMenu}>
+          <img src="/images/JETNL-logo.png" alt="JETNL Logo" className="logo" />
+        </Link>
+      </div>
     </header>
   );
 };
