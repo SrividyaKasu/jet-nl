80dca884ab02e71b8d8d1ea99a3d9155b5e670eb
“Srividyagit config user.email “kasu.srividya@gmail.com <kasu.srividya@gmail.com>
2025-06-12 23:11:39 +0200

    added customer creation

--- api/create-payment-link.js
+++ api/create-payment-link.js
@@ -79,7 +79,9 @@ export default async function handler(req, res) {
         metadata: {
           failure_url: `${req.headers.origin}/payment-failed`
         }
-      }
+      },
+      customer_creation: 'always',
+      ...(email && { customer_email: email })
     });
 
     console.log('Payment link created successfully:', paymentLink.url);
