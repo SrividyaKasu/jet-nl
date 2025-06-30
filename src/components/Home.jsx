7d0fe5790f5253096aef661439a701c8a9f2c5d2
“Srividyagit config user.email “kasu.srividya@gmail.com <kasu.srividya@gmail.com>
2025-06-30 17:42:58 +0200

    updated registration page with venue details

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       </div>
       
       <section className="travel-schedule">
-        <h1>HH Swamiji's NL 2025 Travel Schedule</h1>
+        <h1>HH Chinna Jeeyar Swamiji's NL 2025 Travel Schedule</h1>
         <div className="schedule-table">
           <table>
             <thead>
@@ -18,48 +18,47 @@ const Home = () => {
                 <th>Date</th>
                 <th>Event</th>
                 <th>Location</th>
-                <th>Action</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>July 18th 2025, 5 PM onwards</td>
-                <td>Lakshmi Naryana Pooja</td>
-                <td>US Open Zaal, Bovenkerkerweg 81, 1187 XC Amstelveen</td>
                 <td>
                   <Link 
                     to="/registration?location=amstelveen&program=lakshmi-narayana-pooja" 
-                    className="register-link"
-                  >
-                    Register Now
+                    className="event-button"
+                    title="Register for Lakshmi Naryana Pooja">
+                    Lakshmi Naryana Pooja
+                    <i className="fa fa-sign-in"></i>
                   </Link>
                 </td>
+                <td>US Open Zaal, Bovenkerkerweg 81, 1187 XC Amstelveen</td>
               </tr>
               <tr>
                 <td>July 19th 2025, 5 PM onwards</td>
-                <td>Sita Rama Kalyanam</td>
-                <td>ASAN Arya Samaj Nederland, Van der Kunstraat 75, 2521 BA Den Haag</td>
                 <td>
                   <Link 
                     to="/registration?location=denhaag&program=sita-rama-kalyanam" 
-                    className="register-link"
-                  >
-                    Register Now
+                    className="event-button"
+                    title="Register for Sita Rama Kalyanam">
+                    Sita Rama Kalyanam
+                    <i className="fa fa-sign-in"></i>
                   </Link>
                 </td>
+                <td>ASAN Arya Samaj Nederland, Van der Kunstraat 75, 2521 BA Den Haag</td>
               </tr>
               <tr>
                 <td>July 20th 2025, 1 PM onwards</td>
-                <td>Dhanalakshmi Pooja</td>
-                <td>Eindhoven</td>
                 <td>
                   <Link 
                     to="/registration?location=eindhoven&program=dhana-lakshmi-pooja" 
-                    className="register-link"
-                  >
-                    Register Now
+                    className="event-button"
+                    title="Register for Dhanalakshmi Pooja">
+                    Dhanalakshmi Pooja
+                    <i className="fa fa-sign-in"></i>
                   </Link>
                 </td>
+                <td>Blixems Wijkcentrum, Ouverture 2, 5629 PV Eindhoven</td>
               </tr>
             </tbody>
           </table>
